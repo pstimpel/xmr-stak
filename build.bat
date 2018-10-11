39c50c53a28a4a992d0939706b8f340350990d7c
Peter Stimpel <pstimpel+github@gmail.com>
2018-10-11 23:41:29 +0200

    add some comments in batch file

--- build.bat
+++ build.bat
@@ -6,7 +6,10 @@ rem set(CMAKE_PREFIX_PATH C:\\xmr-stak-dep\\hwloc;C:\\xmr-stak-dep\\libmicrohttp
 mkdir build
 cd build
 
+rem use 141 with depv2, should be ok since there is no collision when not using the GPU stuff
 cmake -G "Visual Studio 15 2017 Win64" -T v141,host=x64 -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF ..
+
+rem use 140 with depv1
 rem cmake -G "Visual Studio 15 2017 Win64" -T v140,host=x64 -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF ..
 
 cmake --build . --config Release --target install
