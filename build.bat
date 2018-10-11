6efde09e1bbad61751eb08923909c15b782dc66c
Peter Stimpel <pstimpel+github@gmail.com>
2018-10-11 23:32:51 +0200

    adapted build.bat, toolchain 141

--- build.bat
+++ build.bat
@@ -6,7 +6,8 @@ rem set(CMAKE_PREFIX_PATH C:\\xmr-stak-dep\\hwloc;C:\\xmr-stak-dep\\libmicrohttp
 mkdir build
 cd build
 
-cmake -G "Visual Studio 15 2017 Win64" -T v140,host=x64 -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF ..
+cmake -G "Visual Studio 15 2017 Win64" -T v141,host=x64 -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF ..
+rem cmake -G "Visual Studio 15 2017 Win64" -T v140,host=x64 -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF ..
 
 cmake --build . --config Release --target install
 cd bin\Release
